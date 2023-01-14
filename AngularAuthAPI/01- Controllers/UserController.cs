491bdedec47dc48dda00d45f2b867ced50d8ca4b
Shonuvik <34462179+Shonuvik@users.noreply.github.com>
2023-01-14 18:17:03 -0300

    Solution implementation

---
+++ AngularAuthAPI/01- Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using AngularAuthAPI.Context1;
+using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(User userObj)
+        {
+            if (userObj is null)
+                return BadRequest();
+
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.UserName == userObj.UserName && x.Password == userObj.Password);
+            if (user is null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj is null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
---
+++ AngularAuthAPI/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using AngularAuthAPI.Commands;
+using AngularAuthAPI.Commands.Interfaces;
+
+namespace AngularAuthAPI.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static void AddIoc(this IServiceCollection services)
+        {
+            services.AddScoped<ICommandResult, CommandResult>();
+        }
+    }
+}
